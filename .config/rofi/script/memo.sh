b8ac838d808ecada4f77423287198d14c08d8b70
44og <t.m.i.h.m4432@gmail.com>
2018-10-05 17:11:21 +0900

    Create memo.sh

---
+++ .config/rofi/script/memo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PATH=${PATH}:${HOME}/local/bin
+
+memo_dir=${TYPORA_MEMO_DIR:-"${HOME}/Documents/typora"}
+list=$(find $memo_dir -type f -name "*.md")
+[ -z "$@" ] && echo "New" 
+[ "$@" = "New" ] && command="typora >/dev/null 2>&1 &"
+for f in $list; do
+  [ -z "$@" ] && echo "$f" && continue
+    [ "$@" = "$f" ] && command="typora $f >/dev/null 2>&1 &" && break
+done
+
+eval "${command:-:}"
